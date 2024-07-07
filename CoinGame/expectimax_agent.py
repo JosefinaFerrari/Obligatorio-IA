4a26f8d17e05504c15687e11bc85b8ea7bce569c
JosefinaFerrari <josefina.ferrari02@gmail.com>
2024-07-07 20:45:57 -0300

    ponderando heuristicas

--- CoinGame/expectimax_agent.py
+++ CoinGame/expectimax_agent.py
@@ -1,14 +1,12 @@
 from board import Board
 from agent import Agent
 
-class MinimaxAgent(Agent):
+class ExpectimaxAgent(Agent):
     def __init__(self, player=1):
         super().__init__(player)
             
     def heuristic_utility(self, board):
-        isolated_coins_utility = self.heuristic_isolated_coins(board)
-        remaining_coins_utility = self.heuristic_remaining_coins(board)
-        return isolated_coins_utility*2/3 - remaining_coins_utility/3
+        return self.heuristic_isolated_coins(board)+self.heuristic_remaining_coins(board)
     
     def heuristic_isolated_coins(self, board):
         isolated_coins = 0
@@ -21,9 +19,9 @@ class MinimaxAgent(Agent):
                             isolated_coins+=1
     
         if(isolated_coins % 2 ==0):
-            return -1*isolated_coins            
+            return -1            
         else: 
-            return isolated_coins
+            return 1
 
     def heuristic_remaining_coins(self, board):
         remaining_coins = 0
@@ -37,13 +35,13 @@ class MinimaxAgent(Agent):
     def next_action(self, obs):
         DEPTH = 3
         playerr = self.player
-        action, _ = self.minimax(board=obs, player=(playerr%2)+1, depth=DEPTH)
+        action, _ = self.expectimax(board=obs, player=(playerr%2)+1, depth=DEPTH)
         if not action:
             possible_actions = obs.get_possible_actions()
             action = possible_actions[0]
         return action
     
-    def minimax(self, board, player, depth):
+    def expectimax(self, board, player, depth):
         action= None
         if depth==0 or board.is_end(player):
             return action, self.heuristic_utility(board)
@@ -61,14 +59,14 @@ class MinimaxAgent(Agent):
                     best_utility = utility
             return best_action, best_utility
 
-        #Minimize other
+        #Expect
         else:
             best_action = None
-            best_utility = float('inf')
+            best_utility = 0
             for action in board.get_possible_actions():
                 new_board = board.clone()
                 new_board.play(action)
-                utility, _ = self.minimax(new_board, (player%2)+1, depth=depth - 1)
+                utility, _ = self.expectimax(new_board, (player%2)+1, depth=depth - 1)
                 if utility < best_utility:
                     best_action = action
                     best_utility = utility
